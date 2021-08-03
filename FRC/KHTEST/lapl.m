11ae319e77a28fe36a45ca92b1370cb530c865a8
Crunch782 <49955727+Crunch782@users.noreply.github.com>
2021-08-03 20:50:16 +0100

    Delete FRC/KHTEST directory

--- FRC/KHTEST/lapl.m
+++
@@ -1,7 +0,0 @@
-% Calculates the laplacian
-
-function lp = lapl(u,im,ic,ip,jm,jc,jp,dx,dy)
-
-    lp = (u(ip, jc)-2*u+u(im,jc))./(dx.^2) + (u(ic,jp)-2*u+u(ic,jm))./(dy.^2);
-
-end
--- FRC/KHTEST/vort.m
+++
@@ -1,7 +0,0 @@
-% Calculates the vorticity field
-
-function w = vort(u,v,dx,dy,im,jc,ic,jm)
-
-    w = (v-v(im,jc))./dx - (u - u(ic,jm))./dy;  
-
-end
