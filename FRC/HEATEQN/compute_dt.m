d718e262af0b91eb881dfeac2d57e77937660c15
Crunch782 <49955727+Crunch782@users.noreply.github.com>
2021-08-03 20:50:02 +0100

    Delete FRC/HEATEQN directory

--- FRC/HEATEQN/compute_dt.m
+++
@@ -1,8 +0,0 @@
-% Compute dt with CFL condition
-
-function dt = compute_dt()
-
-    param;
-    dt = (cfl./2)./(1./(dx.^2) + 1./(dy.^2));
-
-end
--- FRC/HEATEQN/exactsol.m
+++
@@ -1,9 +0,0 @@
-% Computes the Exact Solution for Comparison
-
-function uex = exactsol(X,Y)
-
-    param;
-    
-    uex = cos(a*X).*sin(b*Y);
-
-end
